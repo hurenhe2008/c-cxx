7d7482847d02dd130494b4cbfd8d8934aa358bdf
hu_soul <2280569753@qq.com>
2021-05-05 21:40:27 +0800

    Add files via upload

---
+++ c++11/semaphore.cpp
@@ -0,0 +1,60 @@
+#include <mutex>
+#include <condition_variable>
+
+class semaphore
+{
+public:
+    semaphore(int value = 1) : count_(value), wakeup_(0)
+    {
+
+    }
+
+    ~semaphore()
+    {
+        count_ = 1;
+        wakeup_ = 0;
+    }
+
+    void wait()
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        --count_;
+        if (count_ < 0)
+            cnd_.wait(lock);
+    }
+
+    void signal()
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        ++count_;
+        if (count_ <= 0)
+            cnd_.notify_one();
+    }
+
+    //more fast.
+    void waitP()
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        if (--count_ < 0)
+        {
+            cnd_.wait(lock, [&]()->bool { return wakeup_ > 0; });
+            --wakeup_;   //me wakeup
+        }
+    }
+
+    void signalV()
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        if (++count_ <= 0)
+        {
+            ++wakeup_;
+            cnd_.notify_one();  //have some thread suspend
+        }
+    }
+
+private:
+    int count_;
+    int wakeup_;
+    std::mutex mutex_;
+    std::condition_variable cnd_;
+};
